d8d351eea446dc55715b711a8cd6cd273fd72991
Diana Azizbaeva <diana@mysuperdispatch.com>
2018-12-04 00:10:11 +0500

    Added Before and After Hooks

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,11 +1,36 @@
 var getPalette = require("../lib/getPalette");
 var assert = require("assert");
+var fs = require("fs");
+
+var configFile = process.cwd() + "/config.json";
+
+function writeConfig(config, callback){
+	fs.writeFile(configFile, JSON.stringify(config), callback)
+}
+
 describe("getPalette", function(){
-	it("should throw an error if the result is not in array", function() {
-		assert.throws(getPalette,/is not an array/);
+	
+	var config = {};
+
+	before(function(done){
+		fs.readFile(configFile, function(err, contents){
+			config = JSON.parse(contents.toString());
+			done();
+		});
+	});
+
+	afterEach(function(done){
+		writeConfig(config, done);
+	});
+
+	it("should throw an error if the result is not in array", function(done) {
+		writeConfig({palette: "string"}, function(err) {
+			assert.throws(getPalette,/is not an array/);
+			done();
+		});
 	});
 	
-	it("should return an array with 3 files", function() {
+	it("should return an array with 3 files by default", function() {
 		var palette = getPalette();
 		assert(Array.isArray(palette), "did not return an array");
 		assert.equal(palette.length, 3, "did not return 3 items");
